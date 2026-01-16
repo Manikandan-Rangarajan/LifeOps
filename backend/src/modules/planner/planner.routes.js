3078a653a7ef8b77dbf91b58519d8fc8ed815dba
unknown <username270904@gmail.com>
2026-01-16 22:38:51 +0530

    fixed all the probs related to delete block

--- backend/src/modules/planner/planner.routes.js
+++ backend/src/modules/planner/planner.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createEvent, getEvent,updateEvent, completeEvent } from './planner.controller.js'
+import { createEvent, getEvent,updateEvent, completeEvent, deleteEvent } from './planner.controller.js'
 import {protect} from '../../common/middleware/auth.middleware.js'
 
 const router = express.Router()
@@ -8,4 +8,6 @@ router.post('/',protect,createEvent)
 router.get('/',protect,getEvent)
 router.patch('/:id',protect,updateEvent)
 router.patch("/:id/complete",protect,completeEvent)
+router.delete("/:id",protect,deleteEvent)
+
 export default router
